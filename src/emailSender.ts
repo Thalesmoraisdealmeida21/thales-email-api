a6a17b295de3633d9b6aa71760fa9f147f4d34c5
Thales Morais de Almeida <thales.morais21@gmail.com>
2026-01-18 17:57:52 -0300

    Fixed env vars

--- src/emailSender.ts
+++ src/emailSender.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 
 const user = process.env.EMAIL_USER;
-const pass = process.env.EMAIL_PASS;
+const pass = process.env.EMAIL_PASSWORD;
 const smtpHost = process.env.SMTP_HOST ?? '';
 const smtpPort = Number(process.env.SMTP_PORT) || 587;
 const smtpSecure = process.env.SMTP_SECURE === 'true';
@@ -16,6 +16,14 @@ const transporter = nodemailer.createTransport({
     pass: pass ?? '',
   },
 });
+
+
+console.log('SMTP Host:', smtpHost);
+console.log('SMTP Port:', smtpPort);
+console.log('SMTP Secure:', smtpSecure);
+console.log('Email User:', user);
+console.log('Email Pass:', pass);
+
 
 export async function sendEmail(to: string, subject: string, text: string) {
   try {
